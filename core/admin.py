000811d4a393c849bd1b9639f6bcc6bdcc8e61cb
Marco André Mendes <marcoandre@gmail.com>
2022-09-16 16:24:34 -0300

    models

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
-
-from .models import Categoria, Editora, Usuario
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
+
+from core.models import Autor, Categoria, Editora, Livro, Usuario
 
 
 class UsuarioAdmin(UserAdmin):
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "cpf", "telefone", "data_nascimento")}),
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "cpf",
+                    "telefone",
+                    "data_nascimento",
+                )
+            },
+        ),
         (
             _("Permissions"),
             {
@@ -24,6 +36,9 @@ class UsuarioAdmin(UserAdmin):
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
 
+
+admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Livro)
 admin.site.register(Usuario, UsuarioAdmin)
---
+++ core/views/livro.py
@@ -0,0 +1,16 @@
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Livro
+from core.serializers import LivroDetailSerializer, LivroSerializer
+
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    # serializer_class = LivroSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return LivroDetailSerializer
+        if self.action == 'retrieve':
+            return LivroDetailSerializer
+        return LivroSerializer
