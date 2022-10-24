8abc5aa26dea42ce0697c535dbfe951b3fcdde5f
Marco André Mendes <marcoandre@gmail.com>
2022-10-24 13:07:47 -0300

    settings correção

--- livraria/settings.py
+++ livraria/settings.py
@@ -9,17 +9,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env((os.path.join(BASE_DIR, ".env")))
 
-SECRET_KEY = env("SECRET_KEY")
-DEBUG = env("DEBUG")
-ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(",")
-DATABASES = {"default": env.db()}
-
 if os.getenv("PYTHONANYWHERE_DOMAIN"):
     print("PYTHONANYWHERE_DOMAIN: ", env("PYTHONANYWHERE_DOMAIN"))
     environ.Env.read_env(os.path.join("~/", ".env"))
 else:
     print("PYTHONANYWHERE_DOMAIN: ", None)
     environ.Env.read_env((os.path.join(BASE_DIR, ".env")))
+
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG")
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(",")
+DATABASES = {"default": env.db()}
 
 # Application definition
 INSTALLED_APPS = [
