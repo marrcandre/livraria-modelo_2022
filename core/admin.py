79433923de233281a5b35758f2c77f7a876b3eb4
Marco André Mendes <marcoandre@gmail.com>
2022-11-24 12:47:06 -0300

    Model Compras com Usuário personalizado

--- core/admin.py
+++ core/admin.py
@@ -2,10 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import Autor, Categoria, Editora, Livro, Usuario
-
-# from faker import Faker
-
+from core.models import Autor, Categoria, Compra, Editora, Livro, Usuario
 
 
 @admin.register(Autor)
@@ -14,17 +11,7 @@ class AutorAdmin(admin.ModelAdmin):
     search_fields = ("nome", "email")
     list_filter = ("nome",)
     ordering = ("nome", "email")
-    actions = ['gerar_autores']
 
-    # def gerar_autores(self, request, queryset):
-    #     faker = Faker()
-    #     for _ in range(10):
-    #         Autor.objects.create(
-    #             nome=faker.name(),
-    #             email=faker.email(),
-    #         )
-    #     self.message_user(request, 'Autores gerados com sucesso!')
-    # gerar_autores.short_description = 'Gerar autores'
 
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
@@ -32,6 +19,14 @@ class CategoriaAdmin(admin.ModelAdmin):
     search_fields = ("descricao",)
     list_filter = ("descricao",)
     ordering = ("descricao",)
+
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ("usuario", "status")
+    search_fields = ("usuario", "status")
+    list_filter = ("usuario", "status")
+    ordering = ("usuario", "status")
 
 
 @admin.register(Editora)
---
+++ core/models/compra.py
@@ -0,0 +1,14 @@
+from django.db import models
+from .usuario import Usuario
+
+class Compra(models.Model):
+
+    class StatusCompra(models.IntegerChoices):
+        CARRINHO = 1, 'Carrinho',
+        REALIZADO = 2, 'Realizado',
+        PAGO = 3, 'Pago',
+        ENTREGUE = 4, 'Entregue',
+
+    
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name='compras')
+    status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
