69e454ec7491785ebca3329271c271501d2e7d7e
Marco André Mendes <marcoandre@gmail.com>
2022-11-25 11:28:26 -0300

    Ajustes no status, itens e total

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,10 +1,24 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
-from core.models import Compra
+from core.models import Compra, ItensCompra
+
+
+class ItensCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
+
+    def get_total(self, instance):
+        return instance.livro.preco * instance.quantidade
+
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade", "total")
+        depth = 2
 
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email")
+    status = CharField(source="get_status_display")
+    itens = ItensCompraSerializer(many=True)
 
     class Meta:
         model = Compra
